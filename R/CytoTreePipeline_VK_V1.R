ad7595929316b5cd493a0f84321fc1390bdc8f70
Victor Kreiner <vic.kreiner@live.de>
2023-07-25 15:14:37 +0200

    bk

--- R/CytoTreePipeline_VK_V1.R
+++ R/CytoTreePipeline_VK_V1.R
@@ -64,7 +64,6 @@ SubsampleFCSmatrix <- function(fcs.data = fcs.data,
   
   #subset for final table
   mdl <- mdl[mdl$cell_id %in% rownames(fcs.data),]
-    
 
   message("N cells per file after subsampling: ")
   print(table(gsub("_\\d+$", "", rownames(fcs.data))))
@@ -174,174 +173,83 @@ for(j in 1:length(unique_clusters)){
 return(output.plot)
 }
 
-
-#### QC plots ####
-
-QCplots <- function(FCSlist,
-                    condition_colors = rainbow(20)){
+#### Cluster analysis wrapper ####
+AnalyzeClustersWrapper <- function(cyt = cyt, file.prefix = "CYT_clustering_3-2", wh = 10:10,cell.size = 15){
   
-  ggdf <-  data.frame(FCSlist[["exprs_metadata"]],FCSlist[["exprs_frame"]])
+  #Heatmaps
+plotClusterHeatmap(object = cyt, cluster_rows = FALSE, 
+                     cluster_cols = FALSE, 
+                     markers.to.use = "all", 
+                     scale = "column", 
+                     group.by = "raw_fcs", 
+                     cell.size = cell.size,
+                     pdf.name = paste0(file.prefix,"_HEATMAP_byRawFcs"), #set to "none" to avoid saving plot
+                     wh = wh) #height : width
   
-  ggdf <- pivot_longer(ggdf, cols = c((ncol(FCSlist[["exprs_metadata"]])+1):ncol(ggdf)), names_to = "antigen", values_to = "expression") 
-  
-  pl_qc <- ggplot(ggdf, aes(x = expression, color = condition, 
-                            group = sample_id)) +
-    geom_density() +
-    facet_wrap(~ antigen, nrow = 4, scales = "free") +
-    cowplot::theme_cowplot() +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1), 
-          strip.text = element_text(size = 7), axis.text = element_text(size = 5)) +
-    guides(color = guide_legend(ncol = 1)) +
-    scale_color_manual(values = condition_colors)
-  
-  return(pl_qc)
+plotClusterHeatmap(object = cyt, cluster_rows = TRUE, 
+                     cluster_cols = FALSE, 
+                     markers.to.use = "all", 
+                     scale = "column", 
+                     group.by = "cluster.id", 
+                     cell.size = cell.size,
+                     pdf.name = paste0(file.prefix,"_HEATMAP_byCluster"), #set to "none" to avoid saving plot
+                     wh = wh) #height : width
+                     
+plotClusterHeatmap(object = cyt, cluster_rows = FALSE, 
+                   cluster_cols = FALSE, 
+                   markers.to.use = "all", 
+                   scale = "column", 
+                   group.by = "population", 
+                   cell.size = cell.size,
+                   pdf.name = paste0(file.prefix,"_HEATMAP_byPopulation"), #set to "none" to avoid saving plot
+                   wh = wh) #height : width
+                                        
+  #VLN plots 
+  pdf(paste0(file.prefix,"_VLN_allMarkers_byCluster.pdf"), width = 7, height = 4)
+       for(i in cyt@markers){
+         print(plotViolin(object = cyt, color.by = "cluster.id", marker = i, text.angle = 60) + labs(title = i) + theme(legend.position = "none"))
+       }
+       dev.off()
+                                            
+#FeaturePlots
+pdf(paste0(file.prefix,"_FeatureUMAP_allMarkers.pdf"), width = 7, height = 5)
+for(i in cyt@markers){
+   print(plot2D(object = cyt, item.use = c("UMAP_1", "UMAP_2"), category = "numeric",
+          size = 1, color.by = i) + labs(title = i) + scale_color_viridis_c(option = "A"))
+}
+dev.off()
+                                                
+#Cluster-specific markers (1 cluster vs. all)
+cl_markers <- runDiffFlex(object = cyt, grouping.column = "population", p.adjust.method = "BH")
+openxlsx::write.xlsx(cl_markers,paste0(file.prefix,"_ClusterMarkers.xlsx"))
+                                                                     
+#Pairwise markers (each cluster vs. each other)
+cl_markers <- runDiffFlex(object = cyt, grouping.column = "population", p.adjust.method = "BH", pairwise = TRUE)
+openxlsx::write.xlsx(cl_markers,paste0(file.prefix,"_ClusterMarkersPairwise.xlsx"))
+message("Done")                    
 }
 
-AllvAllplots <- function(FCSlist, subsample = T, 
-                         subsample.factor = 0.1, 
-                         pdf.filename = "traf1", single.plot = TRUE, bins = 150){
+
+#### Template Annotation table ####
+
+SaveTemplateAnnotationTable <- function(cyt = cyt,
+                            out.file = "template.celltypeAnnotation.table.xlsx"){
   
-  if(subsample){
-    ggdf <-  sample_n(FCSlist[["trans_exprs_frame"]], nrow(FCSlist[["trans_exprs_frame"]])*subsample.factor)
-  } else {
-    ggdf <- FCSlist[["trans_exprs_frame"]]
-  }
-  if(single.plot){
-    pdf(paste0("AllvsAll_singlePlot_", pdf.filename, ".pdf"), width = ncol(ggdf), height = ncol(ggdf))
-    print(ggplot(ggdf) +
-            geom_hex(aes(x = .panel_x, y = .panel_y), bins = bins) +
-            facet_matrix(vars(names(FCSlist[["trans_exprs_frame"]]))) + 
-            geom_text(inherit.aes = F,aes(x = .panel_x, y = .panel_y, label = paste0(.panel_x, " X ", .panel_y))) +
-            theme_minimal() + 
-            theme(axis.text = element_blank(), strip.text = element_text(face = "bold")) + 
-            scale_fill_gradientn(colours = c("cadetblue2","gold2","firebrick2"), trans = "log10"))
-    dev.off()
-  }else {
-    combs <- as.data.frame(t(combn(names(ggdf),2)))
-    pdf(paste0("AllvsAll_", pdf.filename, ".pdf"), width = 5, height = 5)
-    for(i in 1:nrow(combs)){
-      print(ggplot(ggdf[,unlist(combs[i,])], aes_string(x = unlist(combs[i,])[1],y = unlist(combs[i,])[2])) +
-              geom_hex(bins = bins) +
-              scale_fill_gradientn(colours = c("#00007F", "blue", "#007FFF", "cyan",
-                                               "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"), trans = "sqrt") + #Colors from Can! 
-              theme_minimal()) 
-    }
-    dev.off()
-  }
+h
+  
+  md.raw <- data.frame("path_fcs" = fcs.files.full,
+                       "raw_fcs" = gsub(".fcs", "",fcs.files), 
+                       "loading_nr" = 1:length(fcs.files),
+                       "sample_id" = fcs.ids[,2],
+                       "population" = unlist(lapply(split(fcs.ids[,2:ncol(fcs.ids)], seq(nrow(fcs.ids))), function(x){
+                         return(gsub(".fcs", "",paste0(x[x != ""], collapse = "_")))
+                       })),
+                       "group_id" = "")
+  
+  openxlsx::write.xlsx(md.raw, file = out.file)
+  
+  
+  
 }
 
-
-#### CATALYST CLUSTER own version ####
-
-cluster <- function (x, 
-                     features = "type", 
-                     xdim = 10, 
-                     ydim = 10, 
-                     maxK = 20, 
-                     verbose = TRUE, 
-                     seed = 1,
-                     stop.at.SOM = FALSE) 
-{
-  stopifnot(is(x, "SingleCellExperiment"))
-  stopifnot(is.logical(verbose), length(verbose) == 1, vapply(list(xdim, 
-                                                                   ydim, maxK, seed), function(arg) is.numeric(arg) && 
-                                                                length(arg) == 1, logical(1)))
-  features <- .get_features(x, features)
-  if (is.null(marker_classes(x))) {
-    rowData(x)$marker_class <- factor(c("state", "type")[as.numeric(rownames(x) %in% 
-                                                                      features) + 1], levels = c("type", "state", "none"))
-  }
-  rowData(x)$used_for_clustering <- rownames(x) %in% features
-  if (verbose) 
-    message("o running FlowSOM clustering...")
-  fsom <- ReadInput(flowFrame(t(assay(x, "exprs"))))
-  som <- BuildSOM(fsom, colsToUse = features, silent = TRUE, 
-                  xdim = xdim, ydim = ydim)
-  if(stop.at.SOM == FALSE){
-    if (verbose) 
-      message("o running ConsensusClusterPlus metaclustering...")
-    pdf(NULL)
-    mc <- suppressWarnings(suppressMessages(ConsensusClusterPlus(t(som$map$codes), 
-                                                                 maxK = maxK, reps = 100, distance = "euclidean", seed = seed, 
-                                                                 plot = NULL)))
-    dev.off()
-    k <- xdim * ydim
-    mcs <- seq_len(maxK)[-1]
-    codes <- data.frame(seq_len(k), map(mc[-1], "consensusClass"))
-    codes <- mutate_all(codes, function(u) factor(u, levels = sort(unique(u))))
-    colnames(codes) <- c(sprintf("som%s", k), sprintf("meta%s", 
-                                                      mcs))
-    x$cluster_id <- factor(som$map$mapping[, 1])
-    metadata(x)$cluster_codes <- codes
-    metadata(x)$SOM_codes <- som$map$codes
-    metadata(x)$delta_area <- .plot_delta_area(mc)
-    return(x)
-  } else {
-    return(som)
-  }
-}
-
-#Solution from Helena Crowell https://github.com/HelenaLC/CATALYST/issues/170
-ReclusterSpec <- function(sce, 
-                          existing.clustering.name = "meta20", 
-                          new.clustering.name = "Newmeta20",
-                          clusters.to.recluster = c(1:10),
-                          features = "type", #passed to CATALYST::cluster
-                          xdim = 10, #passed to CATALYST::cluster
-                          ydim = 10, #passed to CATALYST::cluster
-                          maxK = 20, #passed to CATALYST::cluster
-                          seed = 1234){ #passed to CATALYST::cluster
-  
-  k1 <- existing.clustering.name
-  k2 <- new.clustering.name
-  kids <- clusters.to.recluster
-  
-  sce$cell_id <- seq_len(ncol(sce))
-  sub <- filterSCE(sce, k = k1, cluster_id %in% kids)
-  sub <- CATALYST::cluster(sub, verbose = TRUE, features = features, xdim = xdim, ydim = ydim, maxK = maxK, seed = seed)
-  tmp <- sce
-  # construct new cluster IDs
-  old <- as.character(tmp$cluster_id[sub$cell_id])
-  new <- paste(old, sub$cluster_id, sep = ".")
-  
-  tmp$cluster_id <- as.character(tmp$cluster_id)
-  tmp$cluster_id[sub$cell_id] <- new
-  
-  # construct new cluster codes
-  codes_old <- cluster_codes(tmp)[, c("som100", k1)]
-  codes_new <- cluster_codes(sub)[, c("som100", k2)]
-  
-  ss <- strsplit(unique(new), "\\.")
-  ss <- lapply(ss, as.numeric)
-  codes <- mapply(
-    function(i, j) 
-    {
-      c1 <- codes_old[i, ]
-      c2 <- codes_new[j, ]
-      data.frame(
-        foo = paste(c1$som100, c2$som100, sep = "."),
-        meta = paste(c1[[k1]], c2[[k2]], sep = "."))
-    }, 
-    i = sapply(ss, .subset, 1),
-    j = sapply(ss, .subset, 2),
-    SIMPLIFY = FALSE)
-  codes <- do.call(rbind, codes)
-  
-  # merge old & new cluster codes
-  names(codes) <- names(codes_old)
-  for (i in seq(ncol(codes_old)))
-    codes_old[[i]] <- as.character(codes_old[[i]])
-  codes <- rbind(codes_old, codes)
-  
-  # reorder factor levels
-  for (i in seq(ncol(codes))) {
-    o <- order(as.numeric(unique(codes[[i]])))
-    codes[[i]] <- factor(codes[[i]], unique(codes[[i]])[o])
-  }
-  metadata(tmp)$cluster_codes <- codes
-  
-  print(plotDR(sce, color_by = k1))
-  print(plotDR(tmp, color_by = k1))
-  
-  return(tmp)
-}
+                     
